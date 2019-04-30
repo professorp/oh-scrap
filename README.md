64a8ab7e57592e73706378be86a0035f2492705f
raffleberry <raffleberry42@gmail.com>
2019-04-30 15:55:57 +0530

    openload

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # oh-scrap
-read README.md
+You probably think this repo violates some aspect of the "terms of usage", of some product you own, that your lawyers cooked up.
+
+I got news for you, all code in this repo is for education purposes only. I am not liable if any of this repo's code is used in other projects/app/hacks in any way(piracy?).
+
+If you have any problems, send me a message, I will happily take down the code you're conflicted with(make a "remove" commit).
+
+Thank you for reading this.
---
+++ openload/main.py
@@ -0,0 +1,38 @@
+##recommended header
+from requests import get, head
+from shutil import copyfileobj
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.chrome.options import Options
+
+chrome_options = Options()  
+#chrome_options.add_argument("--headless")
+chrome_options.add_argument("user-data-dir=/home/lunu/.config/google-chrome/Profile 1")
+chrome_options.add_argument("--window-size=1024x768")
+#yes i use adblock
+chrome_options.add_extension('/home/lunu/Apps/ublock/ublock.crx')
+chrome_driver = '/home/lunu/Apps/chromedriver/chromedriver'
+
+browser = webdriver.Chrome(chrome_options=chrome_options, executable_path=chrome_driver)
+
+
+def openload(link):
+  browser.get(link)
+  page_source_len = len(link)
+
+  #this loop is because my isp sometimes blocks the page
+  #remove this if yours doesn't
+  while page_source_len < 10000:
+    browser.get(link)
+    page_source_len = len(browser.page_source)
+
+  browser.find_element_by_css_selector('#btnDl').click()
+  sleep(0.3)
+  browser.find_element_by_css_selector('.dlbutton').click()
+  sleep(0.3)
+  ok_link = browser.find_element_by_css_selector('#realdl a').get_attribute('href')
+  ok = head(link, allow_redirects=False)
+  return ok.headers['Location']
+  sleep(2)
+
